e7cdec937e025e126988c7f96dca7d2502467fc9
rafiechautie <rafiqauti13@gmail.com>
2024-01-07 00:15:52 +0700

    finalization

---
+++ scripts/default-and-named.js
@@ -0,0 +1,10 @@
+export const title = "Programmer Zaman Now"
+
+const content = "Belajar Javascript Module"
+
+const author = "Muhammad Rafie Chautie"
+
+export {
+    content
+}
+export default author
---
+++ scripts/module-object.js
@@ -0,0 +1,13 @@
+export const firstName = "Muhammad"
+export const middleName = "Rafie"
+export const lastName = "Chautie"
+
+export function hello(){
+    console.info("Hello from Hani")
+}
+
+export class Person{
+    constructor(){
+        this.name = "Rafie"
+    }
+}
